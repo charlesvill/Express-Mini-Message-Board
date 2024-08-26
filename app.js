7f2e9d6cd763d98c93f3cee2fa6eb721db19d06b
Charles Villalpando <charlesvill@g.ucla.edu>
2024-08-25 21:20:07 -0700

    restructure for templates that operate simliar to outlets in react

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ const PORT = 3000;
 
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
--- routers/index.js
+++ routers/index.js
@@ -1,19 +1,37 @@
-const { Router }  = require("express");
+const { Router } = require("express");
 
 const indexRouter = Router();
 const addRouter = require("./add");
 
-const messages = [{content: "This is a test message 0"}, {content: "This is a test message 2"}];
+const messages = [{ content: "This is a test message 0" }, { content: "This is a test message 2" }];
 
-indexRouter.get("/", (req, res) => {
-    res.render("index", {messages: messages, greeting: "welcome to the index page!"});
+indexRouter.use("/add", (req, res) => {
+  contentView = "add";
+  res.render("index", {
+    contentView: contentView,
+    params: null
+  });
 });
-
-indexRouter.use("/add", addRouter);
 
 indexRouter.get("/:messageID", (req, res) => {
-    const { messageID } = req.params;
-    res.send(`message id is: ${messageID}`);
+  const { messageID } = req.params;
+  const contentView = "message";
+  res.render("index", {
+    contentView: contentView,
+    params: { messageID: messageID }
+  });
 });
+
+indexRouter.get("/", (req, res) => {
+  contentView = "all";
+  res.render("index", {
+    contentView: contentView,
+    params: { messages: messages }
+  });
+});
+
+
+
+
 // this will be the route for previewing messages
 module.exports = indexRouter;
